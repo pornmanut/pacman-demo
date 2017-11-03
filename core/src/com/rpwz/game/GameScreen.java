e880b14f4d9a69f5f3736b3691388b116e00f414
pornmanut <pornmanuty@gmail.com>
2017-11-03 11:41:26 +0700

    add method to control movement

--- core/src/com/rpwz/game/GameScreen.java
+++ core/src/com/rpwz/game/GameScreen.java
@@ -2,6 +2,7 @@ package com.rpwz.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -23,8 +24,20 @@ public class GameScreen extends ScreenAdapter{
 		Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
+	private boolean isKeyPressed(int key) {
+		if(Gdx.input.isKeyPressed(key))return true;
+		return false;
+	}
+	private void movement() {
+		if(this.isKeyPressed(Keys.LEFT)) {
+			this.x -= 10;
+		}
+		if(this.isKeyPressed(Keys.RIGHT)) {
+			this.x += 10;
+		}
+	}
 	private void update(float delta) {
-		x += 5;
+		this.movement();
 	}
 	@Override
 	public void render(float delta) {
