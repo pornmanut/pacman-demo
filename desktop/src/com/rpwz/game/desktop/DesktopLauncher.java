4bbba67b287a0c140ae711cb6607b38574e8f142
pornmanut <pornmanuty@gmail.com>
2017-11-03 10:37:37 +0700

    init

---
+++ desktop/src/com/rpwz/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rpwz.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rpwz.game.PacmanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PacmanGame(), config);
+	}
+}
