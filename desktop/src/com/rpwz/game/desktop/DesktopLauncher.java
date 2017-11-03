9ba2937b978fa8d4d0b780dcfda599bd1ffe9b8b
pornmanut <pornmanuty@gmail.com>
2017-11-03 10:41:38 +0700

    modify width/height programLauncher

--- desktop/src/com/rpwz/game/desktop/DesktopLauncher.java
+++ desktop/src/com/rpwz/game/desktop/DesktopLauncher.java
@@ -7,7 +7,9 @@ import com.rpwz.game.PacmanGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new PacmanGame(), config);
-		//
+		
 	}
 }
