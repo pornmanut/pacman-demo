0bf372bf3a11f1cab4e82ef8f1ac75ea544a7a8d
pornmanut <pornmanuty@gmail.com>
2017-11-03 17:58:36 +0700

    connect Lancher

--- core/src/com/rpwz/game/PacmanGame.java
+++ core/src/com/rpwz/game/PacmanGame.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PacmanGame extends Game {
 	public SpriteBatch batch;
+	public static final int HEIGHT = 600;
+	public static final int WIDTH = 800;
 	
 	@Override
 	public void create () {
