58ea66aca1a12d04ffd0100672826874afd117d5
pornmanut <pornmanuty@gmail.com>
2017-11-03 16:49:31 +0700

    movement with DIR_OFFSETS

--- core/src/com/rpwz/game/Pacman.java
+++ core/src/com/rpwz/game/Pacman.java
@@ -4,12 +4,21 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Pacman {
 	
+	public static final int DIR_STILL = 0;
 	public static final int DIR_UP = 1;
 	public static final int DIR_RIGHT = 2;
 	public static final int DIR_DOWN = 3;
 	public static final int DIR_LEFT = 4;
-	public static final int DIR_STILL = 0;
-	private static final int moveSpeed = 10;
+
+	private static final int MOVE_SPEED = 10;
+	
+	private static final int [][] DIR_OFFSETS = new int[][] {
+		{0,0},
+		{0,-1},
+		{1,0},
+		{0,1},
+		{-1,0}
+	};
 	
 	private Vector2 position;
 	
@@ -20,20 +29,8 @@ public class Pacman {
 		return position;
 	}
 	public void movement(int dir) {
-		switch(dir) {
-		case DIR_UP:
-			position.y += moveSpeed;
-			break;
-		case DIR_RIGHT:
-			position.x += moveSpeed;
-			break;
-		case DIR_DOWN:
-			position.y -= moveSpeed;
-			break;
-		case DIR_LEFT:
-			position.x -= moveSpeed;
-			break;
-		}
+		position.x += MOVE_SPEED * DIR_OFFSETS[dir][0];
+		position.y += MOVE_SPEED * DIR_OFFSETS[dir][1];
 	}
 	public void update(float delta) {
 	
