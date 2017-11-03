e1c6842ca28e130cac15f1e3d2458c74bd1978ac
pornmanut <pornmanuty@gmail.com>
2017-11-03 17:44:45 +0700

    create class maze to responlity maze.

--- core/src/com/rpwz/game/World.java
+++ core/src/com/rpwz/game/World.java
@@ -2,15 +2,18 @@ package com.rpwz.game;
 
 public class World {
 	private Pacman pacman;
-	//private Maze maze;
+	private Maze maze;
 	
 	public World(PacmanGame pacmanGame){
 		this.pacman = new Pacman(100,100);
-		//this.maze = new Maze();
+		this.maze = new Maze();
 	}
 	
 	
 	public Pacman getPacman() {
 		return pacman;
+	}
+	public Maze getMaze() {
+		return maze;
 	}
 }
